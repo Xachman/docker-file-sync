5619d51df58629de2563ac6d0490fb7725df6d5b
xachman <zironside@hotmail.com>
2017-12-04 02:01:28 +0000

    if sshfs directory is empty stop running

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-run_unison="unison -auto -batch -perms 0 /mnt/droplet /unison"
-run_sftp="sshfs -o allow_other,default_permissions $TARGET /mnt/droplet"
+DROPLET="/mnt/droplet"
+
+run_unison="unison -auto -batch -perms 0 $DROPLET /unison"
+run_sftp="sshfs -o allow_other,default_permissions $TARGET $DROPLET"
 echo "$run_sftp"
 if [ -n "$TARGET" ]; then
     $run_sftp
-    $run_unison
-    while sleep 1; do $run_unison; done
+    if [ "$(ls -A $DROPLET)" ]; then 
+        $run_unison 
+    else
+        exit 1
+    fi
+    while sleep 1; do
+        if [ "$(ls -A $DROPLET)" ]; then 
+            $run_unison 
+        else
+            exit 1
+        fi
+    done
 else
     echo "No TARGET variable"
     exit 1
